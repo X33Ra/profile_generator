96bf265841f1e8feeeb2a567dba52519b796f37f
MrChan <mrkcwayne@gmail.com>
2023-06-12 11:41:13 -0600

    added the array of questions

--- survey.js
+++ survey.js
@@ -9,10 +9,32 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+}); 
+
+const questions = [
+  'What\'s your name? Nicknames are also acceptable :)',
+  'What\'s an activity you like doing?',
+  'What do you listen to while doing that?',
+  'Which meal is your favourite (e.g., dinner, brunch, etc.)?',
+  'What\'s your favourite thing to eat for that meal?',
+  'Which sport is your absolute favourite?',
+  'What is your superpower? In a few words, tell us what you are amazing at!'
+];
 
 rl.question('What do you think of Node.js? ', (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
 
   rl.close();
 });
+
+// this is one big nested callback
+
+// Instead of asking the user about what they think of Node.js, we can ask them the following questions, in order:
+
+// What's your name? Nicknames are also acceptable :)
+// What's an activity you like doing?
+// What do you listen to while doing that?
+// Which meal is your favourite (eg: dinner, brunch, etc.)
+// What's your favourite thing to eat for that meal?
+// Which sport is your absolute favourite?
+// What is your superpower? In a few words, tell us what you are amazing at!
